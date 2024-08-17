992431356f70f80b61706306e9a88a77e61a08b7
mustafa <mmustafayev0001@gmail.com>
2024-08-17 22:20:45 +0400

    error handing added to logactionfilter

--- API/Filters/LogActionFilter.cs
+++ API/Filters/LogActionFilter.cs
@@ -24,50 +24,54 @@ public class LogActionFilter : IAsyncActionFilter
 
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        var httpContext = context.HttpContext;
-        var traceIdentifier = httpContext.TraceIdentifier;
-        var request = httpContext.Request;
-
-        var clientIp = httpContext.Features.Get<IHttpConnectionFeature>()?.RemoteIpAddress?.ToString();
-        var uri = request.Host + request.Path + request.QueryString;
-
-        var token = string.Empty;
-        Guid? userId = null;
-        var authHeaderName = _configSettings.AuthSettings.HeaderName;
-
-        if (!string.IsNullOrEmpty(request.Headers[authHeaderName]) && request.Headers[authHeaderName].ToString().Length > 7)
+        try
         {
-            token = request.Headers[authHeaderName].ToString();
-            userId = !string.IsNullOrEmpty(token) ? _tokenResolverService.GetUserIdFromToken() : null;
-        }
+            var httpContext = context.HttpContext;
+            var traceIdentifier = httpContext.TraceIdentifier;
+            var request = httpContext.Request;
 
-        request.Body.Position = 0;
-        using var streamReader = new StreamReader(request.Body);
-        var bodyContent = await streamReader.ReadToEndAsync();
-        request.Body.Position = 0;
+            var clientIp = httpContext.Features.Get<IHttpConnectionFeature>()?.RemoteIpAddress?.ToString();
+            var uri = request.Host + request.Path + request.QueryString;
 
-        await next();
+            var token = string.Empty;
+            Guid? userId = null;
+            var authHeaderName = _configSettings.AuthSettings.HeaderName;
 
-        var requestLog = new RequestLogDto()
-        {
-            TraceIdentifier = traceIdentifier,
-            ClientIp = clientIp!,
-            Uri = uri,
-            RequestDate = DateTime.Now,
-            Payload = bodyContent,
-            Method = request.Method,
-            Token = token,
-            UserId = userId,
-            ResponseLog = new ResponseLogDto()
+            if (!string.IsNullOrEmpty(request.Headers[authHeaderName]) && request.Headers[authHeaderName].ToString().Length > 7)
+            {
+                token = request.Headers[authHeaderName].ToString();
+                userId = !string.IsNullOrEmpty(token) ? _tokenResolverService.GetUserIdFromToken() : null;
+            }
+
+            request.Body.Position = 0;
+            using var streamReader = new StreamReader(request.Body);
+            var bodyContent = await streamReader.ReadToEndAsync();
+            request.Body.Position = 0;
+
+            await next();
+
+            var requestLog = new RequestLogDto()
             {
                 TraceIdentifier = traceIdentifier,
-                ResponseDate = DateTime.Now,
-                StatusCode = httpContext.Response.StatusCode.ToString(),
+                ClientIp = clientIp!,
+                Uri = uri,
+                RequestDate = DateTime.Now,
+                Payload = bodyContent,
+                Method = request.Method,
                 Token = token,
-                UserId = userId
-            }
-        };
+                UserId = userId,
+                ResponseLog = new ResponseLogDto()
+                {
+                    TraceIdentifier = traceIdentifier,
+                    ResponseDate = DateTime.Now,
+                    StatusCode = httpContext.Response.StatusCode.ToString(),
+                    Token = token,
+                    UserId = userId
+                }
+            };
 
-        await _loggingService.AddLogAsync(requestLog);
+            await _loggingService.AddLogAsync(requestLog);
+        }
+        catch { }
     }
 }
